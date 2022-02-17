bc4736aad6faafca72bb5d17260f3ebf54504ef7
ibrahim.bitar963@gmail.com <ibrahim.bitar963@gmail.com>
2022-02-17 15:13:20 +0200

    json model test

--- lib/features/breaking_characters/data/models/character_model.dart
+++ lib/features/breaking_characters/data/models/character_model.dart
@@ -1,14 +1,22 @@
 import 'package:break_clean/features/breaking_characters/domain/entites/character.dart';
-import 'package:flutter/cupertino.dart';
+
 
 class CharacterModel extends Character {
-  CharacterModel(
-      {required int char_Id,
-      required String nickName,
-      required String actorName})
+  CharacterModel({required int char_Id,
+    required String nickName,
+    required String actorName})
       : super(
-          char_Id: char_Id,
-          actorName: actorName,
-          nickName: nickName,
-        );
+    char_Id: char_Id,
+    actorName: actorName,
+    nickName: nickName,
+  );
+
+
+  factory CharacterModel.fromJson(Map <String, dynamic> json){
+    return CharacterModel(nickName: json['nickname'],
+        actorName: json['name'],
+        char_Id: json['char_id']);
+  }
+
+
 }
--- test/features/breaking_characters/data/models/character_model_test.dart
+++ test/features/breaking_characters/data/models/character_model_test.dart
@@ -1,9 +1,13 @@
 
 
+import 'dart:convert';
+
 import 'package:break_clean/features/breaking_characters/data/models/character_model.dart';
 import 'package:break_clean/features/breaking_characters/domain/entites/character.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+import '../../../../fixture/fixture_reader.dart';
 
 void main (){
 
@@ -12,9 +16,26 @@ test('should get character model',
         ()async*{
 
       expect(tCharacterModel, isA<Character>());
-}
+});
 
-);
+group('from json', (){
+      test('should return a valid model when the json number is ant integer',()async*{
+
+            //arrange
+
+            final Map<String,dynamic> jsonMap=
+                jsonDecode(fixture('characters.json'),);
+
+            //act
+
+            final result = CharacterModel.fromJson(jsonMap);
+            
+            //assert
+            
+            expect(result, tCharacterModel);
 
 
+
+      });
+});
 }
