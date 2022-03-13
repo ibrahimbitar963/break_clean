58fb68579ef06026eac607ff87ff937f75219a6a
ibrahim.bitar963@gmail.com <ibrahim.bitar963@gmail.com>
2022-03-13 11:09:10 +0200

    http list print

--- lib/features/breaking_characters/data/datasources/character_remote_data_source.dart
+++ lib/features/breaking_characters/data/datasources/character_remote_data_source.dart
@@ -1,6 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:break_clean/core/error/exception.dart';
+import 'package:break_clean/features/breaking_characters/data/models/character_model.dart';
+import 'package:break_clean/features/breaking_characters/data/models/character_model.dart';
+import 'package:break_clean/features/breaking_characters/data/models/character_model.dart';
+import 'package:break_clean/features/breaking_characters/domain/entites/character.dart';
 import 'package:http/http.dart'as http;
 import 'package:break_clean/features/breaking_characters/data/models/character_model.dart';
 
@@ -13,7 +17,7 @@ abstract class CharacterRemoteDataSource {
   /// Calls the  https://www.breakingbadapi.com/api/{characters} endpoint.
   ///get all character
   /// Throws a [ServerException] for all error codes.
-  Future<CharacterModel> getAllCharacters();
+  Future<List<dynamic>> getAllCharacters();
 }
 
 class CharacterRemoteDataSourceImpl implements CharacterRemoteDataSource{
@@ -21,20 +25,52 @@ class CharacterRemoteDataSourceImpl implements CharacterRemoteDataSource{
 
   CharacterRemoteDataSourceImpl({required this.client});
   @override
-  Future<CharacterModel> getAllCharacters() {
-    // TODO: implement getAllCharacters
-    throw UnimplementedError();
+  Future<List<dynamic>> getAllCharacters() async {
+    print('object');
+   final response = await client.get(Uri.parse('https://www.breakingbadapi.com/api/characters/'),
+      headers: {'Content-Type': 'application/json'},);
+
+    if (response.statusCode == 200) {
+       List<CharacterModel> values = new List<CharacterModel>.from(
+          json.decode(response.body).map((data) => CharacterModel.fromJson(data)));
+      values.forEach((element) {
+        print(element.nickName);
+      });
+
+       return values;
+    }else {
+      throw Exception('Failed to load ');
+
   }
 
-  @override
-  Future<CharacterModel>? getOneCharacters(int id) async{
-    final response=
-    await client.get(Uri.parse('https://www.breakingbadapi.com/api/characters/$id'),
-    headers: {'Content-Type': 'application/json'},);
-    if(response.statusCode == 200){
-    return CharacterModel.fromJson(jsonDecode(response.body));
-  }else
-  {throw ServerException();}
 
   }
-}
+       @override
+       Future<CharacterModel>? getOneCharacters(int id) async{
+         final response=
+         await client.get(Uri.parse('https://www.breakingbadapi.com/api/characters/$id'),
+           headers: {'Content-Type': 'application/json'},);
+         if(response.statusCode == 200){
+           return CharacterModel.fromJson(jsonDecode(response.body));
+         }else
+         {throw ServerException();}
+
+       }
+
+// Future<List<dynamic>> getAllCharacters() async {
+//   print('object');
+//   try {
+//     final response= await client.get(Uri.parse('https://www.breakingbadapi.com/api/characters/'),
+//       headers: {'Content-Type': 'application/json'},);
+//
+//     //   http.StreamedResponse response = await request.send();
+//     print(response.toString());
+//
+//     var data = json.decode(response.body);
+//     return  data;
+//   } catch (e) {
+//     print('eeeeeeeeeeeee');
+//     print(e.toString());
+//     return [];
+//   }
+ }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:break_clean/features/breaking_characters/data/datasources/character_remote_data_source.dart';
 import 'injection_container.dart' as di;
+import 'injection_container.dart';
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
 
+
   await di.init();
+  print('ssssssssss');
+  di.sl<CharacterRemoteDataSource>().getAllCharacters();
 
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Container(
         child: Text(''),
       ),
